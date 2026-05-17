35ffbc3d79caff2508fa7a8f014d6152ee8afa76
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-17 17:19:32 +0530

    modified

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,68 +10,81 @@ import hero from "../assets/hero.jpg";
 
 function Home() {
   return (
-
     <Box>
 
       {/* HERO SECTION */}
-
       <Box
         sx={{
           height: "90vh",
-
-          backgroundImage:`url(${hero})`,
-           
+          backgroundImage: `url(${hero})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
-
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-
           textAlign: "center",
-
-          color: "white"
+          color: "white",
+          position: "relative"
         }}
       >
 
-        <Container>
+        {/* DARK OVERLAY */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.45)"
+          }}
+        />
+
+        <Container sx={{ position: "relative", zIndex: 2 }}>
 
           <Typography
             variant="h2"
             gutterBottom
             sx={{
-              fontWeight: "bold"
+              fontWeight: "bold",
+              letterSpacing: "2px"
             }}
           >
-            Mystic Knots
+            Mystic-Knots
           </Typography>
 
           <Typography
             variant="h5"
             gutterBottom
+            sx={{
+              opacity: 0.9,
+              fontStyle: "italic"
+            }}
           >
-            Handmade Gifts & Jewelry
+            Handmade Gifts & Jewelry Crafted with Love
           </Typography>
 
           <Button
             variant="contained"
-            size="large"
             component={Link}
             to="/products"
-
             sx={{
               mt: 3,
-
+              backgroundColor: "#b08d57",
+              padding: "10px 25px",
+              fontSize: "16px",
+              borderRadius: "30px",
+              textTransform: "none",
               "&:hover": {
+                backgroundColor: "#8c6f3f",
                 transform: "scale(1.05)"
               }
             }}
           >
-            Shop Now
+            Explore Collection
           </Button>
 
         </Container>
-
       </Box>
 
     </Box>
